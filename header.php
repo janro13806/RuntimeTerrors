88019110aa12e8a7dc2034fc884e0454bff98718
Prometheus18082106 <u21451452@tuks.co.za>
2022-05-27 16:03:03 +0200

    header.php
    Added nav bar code to header.php. This contains links to all the other pages on the website.

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+    // Nav bar that gets included on all pages
+    require_once('config.php');
+        echo '
+            <body>
+            <div class="web-nav-bar" >
+                <ul > 
+                    <li style="width: 100px;"><a  id="Home" href="index.php"><i class="fa-solid fa-house"></i> Home<span>Go to the "Home" page</span></a></li>
+                    <li><a  href="player.php">Players<span>Go to the "Players" page</span></a></li> 
+                    <li><a  href="hole.php">Holes<span>Go to the "Holes" page</span></a></li>
+                    <li><a  href="round.php">Rounds<span>Go to the "Rounds" page</span></a></li>
+                    <li><a  href="statistic.php">Statistics<span>Go to the "Statistics" page</span></a></li>
+                    <li><a  href="tournament.php">Tournaments<span>Go to the "Tournaments" page</span></a></li>
+                    <li><a  href="course.php">Course<span>Go to the "Course" page</span></a></li>
+                </ul>
+            </div>
+        ';
+
+?>
