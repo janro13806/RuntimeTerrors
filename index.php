25124b430f212b4f374263fc20866b1c68dc679c
machiel27 <94852967+machiel27@users.noreply.github.com>
2022-06-02 17:39:02 +0200

    Various Styling added
    Added styling to all table pages and home page. Created a folder for css and imgs.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS\style.css">
 </head>
     <body>
     <?php include 'header.php';?>
@@ -10,8 +11,14 @@
         <p> Here you will find statistics for the last 5 years of the tournament. </p>
         <p> Please use the Navigation Bar at the top of your screen. </p>
 
+        <img id="mastersLogo" src="IMG\masters.jpg" alt="Image not found">
 
-</div>
+
+    </div>
+    <div id="footer">
+				<?php include 'footer.php';?>
+
+	</div>
 
     </body>
 </html>
