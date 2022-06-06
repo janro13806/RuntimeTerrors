6f58cc64f18e6d980803399f9eccf9c39389211c
Prometheus18082106 <u21451452@tuks.co.za>
2022-06-06 23:21:46 +0200

    Tweaked round functions
    Tweaked all the functions in round.js.
    Added a separate request function sendStatRequest to send a request specifically for min/max score.
    Added a displayStat function to display the response for the above mentioned request.

--- JS/round.js
+++ JS/round.js
@@ -1,15 +1,21 @@
 //GET COMPONENTS FROM WITH DOM
 //Get the buttons
-var deleteBtn = document.getElementById("delete");
-deleteBtn.addEventListener("click", deleteCourse());
+var addBtn = document.getElementById("add");
+deleteBtn.addEventListener("click", addScore());
 
-var updateBtn = document.getElementById("update");
-deleteBtn.addEventListener("click", updateCourse());
+var statBtn = document.getElementById("stat");
+deleteBtn.addEventListener("click", getStat());
+var statOption = document.getElementById("stat_option").value;
 
 //Get the inputs
-var idInput = document.getElementById("id_input").value;
-var updateInput = document.getElementById("update_value").value;
-var updateOption = document.getElementById("update_option");
+var round_nr = document.getElementById("round_nr").value;
+var player_id = document.getElementById("player_id").value;
+var tournament_id = document.getElementById("tournament_id").value;
+var score = document.getElementById("score").value;
+var pars = document.getElementById("pars").value;
+var birdies = document.getElementById("birdies").value;
+var bogeys = document.getElementById("bogeys").value;
+
 
 //Get the table
 var table = document.getElementById("dataTable");
@@ -18,36 +24,49 @@ window.addEventListener("load", function(){
 
     //Only set the type
     const data = {
-        "type":"gRound"
+        "type":"gRounds"
     }
 
     sendRequest(data);
 
 });
 
-
-function updateCourse()
+function addScore()
 {
-    //Create data object based on which update option is selected
-   
     const data = {
-        "type":"uCourse",
-        "length":updateInput,
-        "course_id": idInput
+        "type":"aScore",
+        "player_id": player_id,
+        "tournament_id": tournament_id,
+        "round_nr": round_nr,
+        "score": score,
+        "pars": pars,
+        "birdies": birdies,
+        "bogeys": bogeys,
     };
-    
-    
+
     sendRequest(data); 
 }
 
-function deleteCourse()
-{
-    const data = {
-        "type":"dCourse",
-        "course_id": idInput
-    };
 
-    sendRequest(data); 
+function getStat()
+{
+    //Create data object with type min/max depending on which option is selected
+    if (statOption == "Minimum")
+    {
+        const data = {
+            "type":"maxScore"
+        };
+    }
+    else
+    {
+        const data = {
+            "type":"minScore"
+        };
+    
+    }
+    
+
+    sendStatRequest(data); 
 }
 
 
@@ -94,11 +113,13 @@ function displayTable(resBody)
 
             tableText += "<tr>";
 
-            tableText += "<td>" + item.course_id + "</td>";
-            tableText += "<td>" + item.name + "</td>"; 
-            tableText += "<td>" + item.town + "</td>";
-            tableText += "<td>" + item.city + "</td>";
-            tableText += "<td>" + item.length + "</td>";
+            tableText += "<td>" + item.round_nr + "</td>";
+            tableText += "<td>" + item.player_id + "</td>"; 
+            tableText += "<td>" + item.round_nr + "</td>";
+            tableText += "<td>" + item.score + "</td>";
+            tableText += "<td>" + item.pars + "</td>";
+            tableText += "<td>" + item.birdies + "</td>";
+            tableText += "<td>" + item.bogeys + "</td>";
 
             tableText += "</tr>";
         }
@@ -108,5 +129,47 @@ function displayTable(resBody)
 
         //Add the contents to the table
         table.innerHtml = tableText;
+    }
+}
+
+//SEND REQUEST SPECIFICALLY FOR MIN OR MAX
+function sendStatRequest(data)
+{
+    //Create the request
+    const xhttpr = new XMLHttpRequest();
+
+    //Set how the response is handled
+    xhttpr.addEventListener("readystatechange", function() {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            var res = xhttpr.responseText;
+            console.log(res);
+            displayStat(res);
+        }
+    });
+
+
+    // console.log(data);
+
+    //Set the method and the URL
+    xhttpr.open("POST", "../api.php");
+
+    //Send the request with data as the body
+    xhttpr.send(JSON.stringify(data));
+}
+
+function displayStat(resBody)
+{
+    var resData = JSON.parse(resBody);
+
+    if (resData.success == "success")
+    {
+        //Parse the data part of the response object
+        var arrData = JSON.parse(resData.message);
+
+
+        //Display the stat in one of the html objects
+        
+
     }
 }
