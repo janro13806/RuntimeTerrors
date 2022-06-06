2592b7b7b16f261a09e0301b29542032f928bf32
janro13806 <botha.janro@gmail.com>
2022-06-07 00:50:09 +0200

    backend functions
    Work on Backend functions.
    
    Co-Authored-By: Piery van der Linde <105210148+pieryvdl@users.noreply.github.com>
    Co-Authored-By: Daniel van Zyl <88310606+DvZ02@users.noreply.github.com>

--- functions.php
+++ functions.php
@@ -4,79 +4,280 @@
 //connect to db
 include "config.php";
 
-function getPlayers() {
+//get the players of the tournament
+function getPlayer() {
     $query = "
-    SELECT name,surname,nationality,weight,height,age 
-    FROM person";
+    SELECT p.person_id AS Person_id ,p.name AS Name,p.surname AS Surname,
+    pl.player_id AS Player_ID,p.nationality AS Nationality,
+    p.weight AS Weight,p.height AS Height,
+    p.age AS Age,pl.rank AS Rank
+    FROM person p, player pl";
     $queryresult = $con->query($query);
+    $row = $queryresult->fetch_assoc();
+
+    //Code to build assoc array
+    $index = 0;
+    $assocArr = [];
+    while($row = fetch_assoc($queryresult)){
+       $assocArr[$index++] = array("Person_id" => $row["Person_id"],"Name" => $row["Name"]  
+    ,"Surname" => $row["Surname"], "Player_ID" => $row["Player_ID"], 
+    "Nationality" => $row["Nationality"], "Weight" => $row["Weight"], 
+    "Height" => $row["Height"], "Age" => $row["Age"], "Rank" => $row["Rank"]); 
+    }
+    
+    return $assocArr;
 }
 
-//get statistics table
-function getPlayersStats(){
+//get the course
+function getCourse(){
     $query = "
-    SELECT s.statistic_id,s.tournament,s.roun_nr,s.score,s.pars,s.birdies,s.bogeys 
-    FROM statistic s,";
+    SELECT *
+    FROM course";
     $queryresult = $con->query($query);
-}
+    
+    //Code to build assoc array
+    $index = 0;
+    $assocArr = [];
+    while($row = fetch_assoc($queryresult))
+    {
+        $assocArr[$index++] = array(
+            "Course_Id" => $row["course_id"],
+            "Name" => $row["name"],
+            "Town" => $row["town"],
+            "City" => $row["city"],
+            "Length" => $row["length"]
+        );
+    }
 
-//get the tournament
-function getTournament(){
-    $query = "
-    SELECT year,tournament_id,win_score,winner,course_id
-    FROM tournament";
-    $queryresult = $con->query($query);
+    return $assocArr;
 }
 
 //get the holes of the course
 function getHoles(){
     $query = "
-    SELECT t.hole_nr,t.distance,t.par,t.num_bunkers,t.stroke_difficulty
-    FROM tournament t,course c
-    WHERE t.course_id=c.course_id";
+    SELECT *
+    FROM hole";
     $queryresult = $con->query($query);
+
+     //Code to build assoc array
+     $index = 0;
+     $assocArr = [];
+     while($row = fetch_assoc($queryresult))
+     {
+         $assocArr[$index++] = array(
+             "Hole_nr" => $row["hole_nr"],
+             "Distance" => $row["distance"],
+             "Par" => $row["par"],
+             "Num_Bunkers" => $row["num_bunkers"],
+             "Stroke_Difficulty" => $row["stroke_difficulty"]
+             "Course_id" => $row["course_id"]
+         );
+     }
+ 
+     return $assocArr;
 }
 
+//get the rounds 
 function getRounds(){
     $query = "
-    SELECT player_id,tournament_id,round_nr,score
+    SELECT *
     FROM round";
     $queryresult = $con->query($query);
+    
+         //Code to build assoc array
+         $index = 0;
+         $assocArr = [];
+         while($row = fetch_assoc($queryresult))
+         {
+             $assocArr[$index++] = array(
+                "Tournament_id" => $row["tournament_id"],
+                "Player_id" => $row["player_id"],
+                "Round_nr" => $row["round_nr"],
+                "Score" => $row["score"],
+             );
+         }
+         return $assocArr;
 } 
 
-function getCourse(){
+//get statistics table
+function getStatistics(){
     $query = "
-    SELECT course_id,name,town,city,length
-    FROM course";
+    SELECT *
+    FROM statistic";
     $queryresult = $con->query($query);
+
+    $index = 0;
+    $assocArr = [];
+    while($row = fetch_assoc($queryresult))
+    {
+        $assocArr[$index++] = array(
+            "Statistic_Id" => $row["statistic_id"],
+            "Tournament_Id" => $row["tournament_id"],
+            "Round_Nr" => $row["round_nr"],
+            "Score" => $row["score"],
+            "Pars" => $row["Pars"],
+            "Birdies" => $row["birdies"],
+            "Bogeys" => $row["bogeys"],
+        );
+    }
+    return $assocArr;
 }
 
-//this function needs to be checked. It is intended for a spesific player
-function getIntendedPlayerStat($playerid){
+//get the tournament
+function getTournaments(){
     $query = "
-    SELECT p.name,p.surname,s.round,s.score,s.pars,s.birdies,s.bogeys 
-    FROM statistic s,person p,round_statistic r,player pl
-    WHERE (r.statistic_id = s.statistic_id AND r."+$playerid+"=pl.playerid) ";
+    SELECT *
+    FROM tournament";
     $queryresult = $con->query($query);
+
+    $index = 0;
+    $assocArr = [];
+    while($row = fetch_assoc($queryresult))
+    {
+        $assocArr[$index++] = array(
+            "Tournament_Id" => $row["tournament_id"],
+            "Winner_Score" => $row["win_score"],
+            "Winner" => $row["winner"],
+            "Course" => $row["course_id"]
+        );
+    }
+
+    return $assocArr;
 }
 
-//delete a player Important: Player has statistics,rounds etc to be deleted
-function removePlayer(){
-    $playerid = "";
+//================================================================================
+//================================================================================
+//================================================================================
+
+function deletePlayer($playerid){
     $query = "
-    DELETE FROM player 
-    WHERE playerid="+$playerid+"";
-    $queryresult = $con->query($query);
+    DELETE FROM player
+    WHERE player_id="+$playerid+"";
+    
+    if ($con->query($query)) {
+        return getPlayer();
+    }
+    else {
+        return false;
+    }
+
+    
 }
 
-//add a person to the table
-function addPerson($personid,$name,$surname,$nationality,$weight,$height,$age){
+function updatePlayerWeight($player_id,$weight){
     $query = "
-    INSERT INTO person 
-    VALUES ("+$personid+","+$name+$surname+","+$nationality+","+$weight+","+$height+","+$age+")";
-    $queryresult = $con->query($query);
+    UPDATE person
+    SET weight="+$weight+"
+    WHERE person_id = ( 
+        SELECT person_id
+        FROM player
+        WHERE player_id="+$player_id+")";
+
+    if ($con->query($query)) {
+        return getPlayer();
+    }
+    else {
+        return false;
+    }
+}
+
+function updatePlayerAge($player_id,$age){
+    $query = "
+    UPDATE person
+    SET age="+$age+"
+    WHERE person_id = ( 
+        SELECT person_id
+        FROM player
+        WHERE player_id="+$player_id+")";
+
+    if ($con->query($query)) {
+        return getPlayer();
+    }
+    else {
+        return false;
+    }
+}
+
+function updateCourse($length,$course_id){
+    $query = "
+    UPDATE FROM player
+    SET length="+$length+"
+    WHERE course_id="+$course_id+"";
+
+    if ($con->query($query)) {
+        return getCourse();
+    }
+    else {
+        return false;
+    }
+}
+
+function deleteCourse($course_id){
+    $query = "
+    DELETE FROM course
+    WHERE course="+$course_id+"";
+
+    if ($con->query($query)) {
+        return getCourse();
+    }
+    else {
+        return false;
+    }
+}
+
+function addScore($player_id, $tournament_id, $round_nr, $score, $pars, $birdies, $bogeys){
+    $getStatIdQuery = "SELECT statistic_id FROM statistic ORDER BY statistic_id DESC LIMIT 1";
+    $getStatIdResult = $con->($getStatIdQuery);
+    
+    $row = $getStatIdResult->fetch_assoc();
+    $newStatId = $row["statistic_id"] + 1;
+
+    $query = "
+        INSERT INTO round VALUES("+ $player_id +", "+ $tournament_id +", "+ $round_nr +", "+ $score +");
+        INSERT INTO statistic VALUES("$newStatId" ,"+ $player_id +", "+ $tournament_id +", "+ $round_nr +", "+ $score +", "+ $pars +","+ $birdies +", "+ $bogeys +");
+        INSERT INTO round_statistic VALUES("$newStatId","+ $player_id +", "+ $round_nr +");
+    ";
+
+    if($con->query($query)){
+        return getRounds(); //possibly getStatistics() depending on the display page
+    }else{
+        return false;
+    }
 }
 
 
+function countPlayers(){
+    $query = "
+    SELECT COUNT(player_id)
+    AS PlayerCount
+    FROM player";
 
+    $queryresult = $con->query($query);
+    $row = $queryresult->fetch_assoc();
+    return $row['PlayerCount'];
+}
+
+
+function minScore(){
+    $query = "
+    SELECT MIN(Score)
+    AS LowestScore
+    FROM round";
+      
+    $queryresult = $con->query($query);
+    $row = $queryresult->fetch_assoc();
+    return $row['LowestScore'];
+}
+
+function maxScore(){
+    $query = "
+    SELECT MAX(Score)
+    AS HighestScore
+    FROM round";
+      
+    $queryresult = $con->query($query);
+    $row = $queryresult->fetch_assoc();
+    return $row['HighestScore'];
+}
 
 ?>
