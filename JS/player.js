736e8569b3348a7f8181f464d0b9bd025088b2f9
Prometheus18082106 <u21451452@tuks.co.za>
2022-06-07 19:24:12 +0200

    Player table
    Added new code to build the player table correctly.
    Fixed table css

--- JS/player.js
+++ JS/player.js
@@ -10,13 +10,16 @@
 // var idInput = document.getElementById("id_input");
 // var updateInput = document.getElementById("update_value").value;
 
+// var weightBtn = document.getElementById("weight_option");
+// var ageBtn = document.getElementById("age_option");
 
 // var weightOption = document.getElementById("weight_option");
 // var ageOption = document.getElementById("age_option");
 // console.log(ageOption);
 
 //Get the table
-var table = document.getElementById("dataTable");
+var table = document.querySelector("#dataTable");
+table.innerHTML = "";
 console.log(table);
 
 window.addEventListener("load", function(){
@@ -31,26 +34,44 @@ window.addEventListener("load", function(){
 });
 
 
-function updatePlayer()
-{
-    //Create data object based on which update option is selected
-    if (ageOption.checked == true)
-    {
-        const data = {
-            "type":"uPlayer",
-            "age":updateInput,
-            "player_id": idInput
-        };
-    }
-    else
-    {
-        const data = {
-            "type":"uPlayer",
-            "weight":updateInput,
-            "player_id": idInput
-        };
-    }
+// function updatePlayer()
+// {
+//     //Create data object based on which update option is selected
+//     if (ageOption.checked == true)
+//     {
+        
+//     }
+//     else
+//     {
+//         const data = {
+//             "type":"uPlayer",
+//             "weight":updateInput,
+//             "player_id": idInput
+//         };
+//     }
     
+//     sendRequest(data); 
+// }
+
+function age()
+{
+    const data = {
+        "type":"uPlayer",
+        "age":updateInput,
+        "player_id": idInput
+    };
+
+    sendRequest(data); 
+}
+
+function weight()
+{
+    const data = {
+        "type":"uPlayer",
+        "weight":updateInput,
+        "player_id": idInput
+    };
+
     sendRequest(data); 
 }
 
@@ -71,6 +92,11 @@ function countPlayers()
     };
 
     sendCountRequest(data);
+}
+
+function addPlayer()
+{
+
 }
 
 
@@ -112,32 +138,66 @@ function displayTable(resBody)
 
         var arrData = resData.message;
 
-        var tableText = "";
+        // var tableText = "";
 
         //Loop through the array
         for (let i = 0; i < arrData.length; i++) {
             const item = arrData[i];
 
-            console.log(item.Player_ID);
-            tableText += "<tr>";
+            tr = document.createElement('tr');
+            td1 = document.createElement('td');
+            td2 = document.createElement('td');
+            td3 = document.createElement('td');
+            td4 = document.createElement('td');
+            td5 = document.createElement('td');
+            td6 = document.createElement('td');
+            td7 = document.createElement('td');
+            td8 = document.createElement('td');
 
-            tableText += "<td>" + item.Player_ID + "</td>";
-            tableText += "<td>" + item.Rank + "</td>";  //Should probably add a table heading for the rank
-            tableText += "<td>" + item.Name + "</td>";
-            tableText += "<td>" + item.Surname + "</td>";
-            tableText += "<td>" + item.Age + "</td>";
-            tableText += "<td>" + item.Nationality + "</td>";
-            tableText += "<td>" + item.Weight + "</td>";
-            tableText += "<td>" + item.Height + "</td>";
+            td1.textContent = item.Player_ID;
+            td2.textContent = item.Rank;
+            td3.textContent = item.Name;
+            td4.textContent = item.Surname;
+            td5.textContent = item.Age;
+            td6.textContent = item.Nationality;
+            td7.textContent = item.Weight;
+            td8.textContent = item.Height;
+           
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+            tr.appendChild(td8);
 
-            tableText += "</tr>";
+            table.appendChild(tr);
+
+            // console.log(item.Player_ID);
+
+
+            // tableText += "<tr>";
+
+            // tableText += "<td>" +  + "</td>";
+            // tableText += "<td>" + item.Rank + "</td>";  //Should probably add a table heading for the rank
+            // tableText += "<td>" + item. + "</td>";
+            // tableText += "<td>" + item. + "</td>";
+            // tableText += "<td>" + item. + "</td>";
+            // tableText += "<td>" + item. + "</td>";
+            // tableText += "<td>" + item.Weight + "</td>";
+            // tableText += "<td>" + item.Height + "</td>";
+
+            // tableText += "</tr>";
         }
 
         // tableText += "</table>";
 
 
         //Add the contents to the table
-        console.log(tableText);
-        table.innerHtml = tableText;
+        // console.log(tableText);
+        // table.innerHtml = "";
+        // table.innerHtml += tableText;
+        
     }
 }
--- player.php
+++ player.php
@@ -19,9 +19,11 @@
    </div>
    <br><br><br>
    <div id="main">
-   <table id = "dataTable">
+   <table >
+      <thead>
       <tr>
          <th>ID</th>
+         <th>Rank</th>
          <th>Name</th>
          <th>Surname</th>
          <th>Age</th>
@@ -30,6 +32,9 @@
          <th>Height</th>
 
       </tr>
+      </thead>
+      <tbody id = "dataTable">
+      
       <tr>
          <td>1</td>
          <td>Tiger</td>
@@ -48,17 +53,19 @@
          <td>78kg</td>
          <td>1.8m</td>
       </tr>
+      </tbody>
+      
    </table> 
    <br><br><br><br><br><br><br><br><br><br><br><br>
    </div>
 
    <div id="playerContainer">
    <div id="deletePlayer" class="controls">
-   <button class="playerButton" type="button" id="deleteButton">Delete Player</button><br>
+   <button class="playerButton" type="button" id="deleteButton" onclick="deletePlayer()">Delete Player</button><br>
    <input type="text" placeholder="Player ID" name="name" id="id_input"/><br>
    </div>
    <div id="addPlayer" class="controls">
-   <button class="playerButton" type="button" id="add">Add Player</button>
+   <button class="playerButton" type="button" id="add" onclick="addPlayer()">Add Player</button>
    <form action="">
          <input type="text" placeholder="First Name" name="name"/><br>
          <input type="text" placeholder="Surname" name="name"/><br>
@@ -71,8 +78,8 @@
    </div>
    <div id="changePlayer" class="controls">
    <button class="playerButton" type="button" id="changeButton">Change Attribute Value</button><br>
-   <button class="playerButton" type="button" id="min">Minimum Score</button>
-   <button class="playerButton" type="button" id="max">Maximum Score</button>
+   <button class="playerButton" type="button" id="weight" onclick="weight()">Weight</button>
+   <button class="playerButton" type="button" id="age" onclick="age()">Age</button>
    <!-- <input type="radio" name="update_option" value="Weight" id="weight_option" checked> Weight<br>
    <input type="radio" name="update_option" value="Height" id="age_option"> Height<br> -->
    <input type="text" placeholder="Value" name="name" id="update_value"/><br>
