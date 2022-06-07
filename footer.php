4b172511e2315e11d99f567171a04d9d1613ed75
MuzzaG <u20440716@tuks.co.za>
2022-06-07 16:14:43 +0200

    Changes to Footer and Player

--- footer.php
+++ footer.php
@@ -1,8 +1,44 @@
-<?php
-        echo '
-            <div class="footer" >
-                <p> Project by: RuntimeTerrors </p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="footer-container">
+        <div class="footer">
+            <div class="footer-heading footer-1">
+                <h2>About Us</h2>
+                <a href="#">About Us</a>
+                <a href="#">Terms of Use</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Accessibility</a>
+                <a href="#">Newsletter</a>
             </div>
-        ';
-
-?>
+            <div class="footer-heading footer-2">
+                <h2>Contact Us</h2>
+                <a href="#">Jobs</a>
+                <a href="#">Support</a>
+                <a href="#">Contact</a>
+                <a href="#">Sponsorships</a>
+            </div>
+            <div class="footer-heading footer-3">
+                <h2>Social Media</h2>
+                <a href="#">Instagram</a>
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">YouTube</a>
+            </div>
+            <div class="footer-email-form">
+                <h2>Join Our Newsletter</h2>
+                <input type="email" placeholder="Enter your email address" id="footer-email">
+                <input type="submit" id="footer-email-btn" value="Sign Up" />
+            </div>
+            <p>©2022 Global News Media. A Botha News Company. All Rights Reserved.</p>
+        </div>
+    </div>
+</body>
+</html>
